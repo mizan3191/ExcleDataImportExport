4e1274b668886f3a8db282d37c2727a23a93a36d
Mizanur Rahman <mizanurrahaman020195@gmail.com>
2022-01-04 18:29:25 +0600

    save

---
+++ src/DataImporter/DataImporter.Membership/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace DataImporter.Membership.Entities
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public string FullName { get; set; }
+    }
+}
